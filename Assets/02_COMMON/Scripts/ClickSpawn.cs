c95d3f22a6e58a89de87d613d41b611fb8325285
Mercan Onur <mercanonurer@gmail.com>
2023-11-12 18:49:32 +0300

    initial commit

---
+++ Assets/02_COMMON/Scripts/ClickSpawn.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickSpawn : MonoBehaviour
+{
+    public GameObject objectToSpawn;
+
+    public float duration;
+    public float targetScale;
+
+
+
+    private IEnumerator TweenObject(GameObject orb)
+    {
+        float timer = 0;
+        orb.transform.localScale = Vector3.zero;
+        while (timer < duration)    
+        {
+            
+            orb.transform.localScale = Vector3.Lerp(Vector3.zero, Vector3.one*targetScale, timer);
+            timer += Time.deltaTime;
+            yield return null;
+        }
+
+        Destroy(orb);
+       
+
+    }
+    private IEnumerator Dissolve(MeshRenderer mr)
+    {
+        float timer = 0;
+        float value = mr.materials[1].GetFloat("_DissolveDistance");
+
+        if (value > -0.2f) { }
+        else 
+        {
+            while (timer < 1)
+            {
+
+                ;
+                value = Mathf.LerpUnclamped(-0.2f, 1.15f, timer);
+                mr.materials[1].SetFloat("_DissolveDistance", value);
+
+                timer += Time.deltaTime;
+                yield return null;
+            }
+        }
+
+        
+
+       
+
+
+    }
+    void Update()
+    {
+        // Ekran üzerinde bir dokunma algılandı mı kontrol et
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                // Dokunulan noktada objeyi oluştur
+                GameObject orb = Instantiate(objectToSpawn, hit.point, Quaternion.identity);
+                StartCoroutine(TweenObject(orb));
+                StartCoroutine(Dissolve(hit.collider.gameObject.GetComponent<MeshRenderer>()));
+
+
+            }
+
+
+        }
+    }
+}
